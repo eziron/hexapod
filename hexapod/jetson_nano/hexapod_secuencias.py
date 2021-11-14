822863bd965ebfbe6755382b5c11c6175e5d80e9
eziron <kraedon.rf@gmail.com>
2021-11-14 05:45:42 +0100

    hexapod baile

--- hexapod/jetson_nano/hexapod_secuencias.py
+++ hexapod/jetson_nano/hexapod_secuencias.py
@@ -6,53 +6,57 @@ from servo_carteciano import Hexapod
 
 secuencia = [
     [
-        [ 210.0, 285.0,   0.0], #[0] pie 1
-        [ 300.0,   0.0,   0.0], #[1] pie 2
+        [ 100.0, 350.0, 100.0], #[0] pie 1
+        [ 250.0, 100.0,   0.0], #[1] pie 2
         [ 210.0,-285.0,   0.0], #[2] pie 3
         [-210.0,-285.0,   0.0], #[3] pie 4
-        [-300.0,   0.0,   0.0], #[4] pie 5
-        [-210.0, 285.0,   0.0], #[5] pie 6
-        [   0.0,   0.0,   5.0], #[6] Rotaciones
-        [   0.0,   0.0,   0.0], #[7] punto de rotacion
-        [  20.0,   0.0,   0.0], #[8] desplazamiento simple
-        60.0, #[9] H 
-        0.3   #[10] step time
-    ],[
-        [ 210.0, 285.0,   0.0], #[0] pie 1
-        [ 300.0,   0.0,   0.0], #[1] pie 2
-        [ 210.0,-285.0,   0.0], #[2] pie 3
-        [-210.0,-285.0,   0.0], #[3] pie 4
-        [-300.0,   0.0,   0.0], #[4] pie 5
-        [-210.0, 285.0,   0.0], #[5] pie 6
-        [   0.0,   0.0,   0.0], #[6] Rotaciones
-        [   0.0,   0.0,   0.0], #[7] punto de rotacion
-        [   0.0,   0.0,  10.0], #[8] desplazamiento simple
-        60.0, #[9] H 
-        0.3   #[10] step time
-    ],[
-        [ 210.0, 285.0,   0.0], #[0] pie 1
-        [ 300.0,   0.0,   0.0], #[1] pie 2
-        [ 210.0,-285.0,   0.0], #[2] pie 3
-        [-210.0,-285.0,   0.0], #[3] pie 4
-        [-300.0,   0.0,   0.0], #[4] pie 5
-        [-210.0, 285.0,   0.0], #[5] pie 6
+        [-250.0, 100.0,   0.0], #[4] pie 5
+        [-100.0, 350.0, 100.0], #[5] pie 6
         [   0.0,   0.0,  -5.0], #[6] Rotaciones
         [   0.0,   0.0,   0.0], #[7] punto de rotacion
-        [ -20.0,   0.0,   0.0], #[8] desplazamiento simple
-        60.0, #[9] H 
-        0.3   #[10] step time
+        [  20.0,   0.0,   0.0], #[8] desplazamiento simple
+        [False,True,True,True,True,False],#[9] desplazamientos y rotaciones
+        80.0, #[10] H 
+        0.25   #[11] step time
     ],[
-        [ 210.0, 285.0,   0.0], #[0] pie 1
-        [ 300.0,   0.0,   0.0], #[1] pie 2
+        [ 100.0, 350.0, 100.0], #[0] pie 1
+        [ 250.0, 100.0,   0.0], #[1] pie 2
         [ 210.0,-285.0,   0.0], #[2] pie 3
         [-210.0,-285.0,   0.0], #[3] pie 4
-        [-300.0,   0.0,   0.0], #[4] pie 5
-        [-210.0, 285.0,   0.0], #[5] pie 6
+        [-250.0, 100.0,   0.0], #[4] pie 5
+        [-100.0, 350.0, 100.0], #[5] pie 6
         [   0.0,   0.0,   0.0], #[6] Rotaciones
         [   0.0,   0.0,   0.0], #[7] punto de rotacion
-        [   0.0,   0.0,  10.0], #[8] desplazamiento simple
-        60.0, #[9] H 
-        0.3   #[10] step time
+        [   0.0,   0.0,  20.0], #[8] desplazamiento simple
+        [False,True,True,True,True,False],#[9] desplazamientos y rotaciones
+        80.0, #[10] H 
+        0.3   #[11] step time
+    ],[
+        [ 100.0, 350.0, 100.0], #[0] pie 1
+        [ 250.0, 100.0,   0.0], #[1] pie 2
+        [ 210.0,-285.0,   0.0], #[2] pie 3
+        [-210.0,-285.0,   0.0], #[3] pie 4
+        [-250.0, 100.0,   0.0], #[4] pie 5
+        [-100.0, 350.0, 100.0], #[5] pie 6
+        [   0.0,   0.0,   5.0], #[6] Rotaciones
+        [   0.0,   0.0,   0.0], #[7] punto de rotacion
+        [ -20.0,   0.0,   0.0], #[8] desplazamiento simple
+        [False,True,True,True,True,False],#[9] desplazamientos y rotaciones
+        80.0, #[10] H 
+        0.25   #[11] step time
+    ],[
+        [ 100.0, 350.0, 100.0], #[0] pie 1
+        [ 250.0, 100.0,   0.0], #[1] pie 2
+        [ 210.0,-285.0,   0.0], #[2] pie 3
+        [-210.0,-285.0,   0.0], #[3] pie 4
+        [-250.0, 100.0,   0.0], #[4] pie 5
+        [-100.0, 350.0, 100.0], #[5] pie 6
+        [   0.0,   0.0,   0.0], #[6] Rotaciones
+        [   0.0,   0.0,   0.0], #[7] punto de rotacion
+        [   0.0,   0.0,  20.0], #[8] desplazamiento simple
+        [False,True,True,True,True,False],#[9] desplazamientos y rotaciones
+        80.0, #[10] H 
+        0.3   #[11] step time
     ]
 ]
 
@@ -68,20 +72,26 @@ def bucle_movimiento():
         msg_tx = struct.pack("<"+"H"*18,*duty_vals)
         Serial.write(msg_tx)
         msg_rx = Serial.readline()
+        if(msg_rx is None):
+            print("raspberry pi pico no responde")
 
 hexapod.reset_dt()
 
-hexapod.set_param_time(2,h=60)
+for i in range(6):
+    hexapod.lineal_set_target_time(i,secuencia[0][i],1,False)
+bucle_movimiento()
+
+hexapod.set_param_time(1,h=secuencia[0][10])
 bucle_movimiento()
 
 for n in range(30):
     for x in secuencia:
         for i in range(6):
-            hexapod.lineal_set_target_time(i,x[i],x[10])
+            hexapod.lineal_set_target_time(i,x[i],x[11],x[9][i])
         
         hexapod.set_param_time(
-                time=x[10],
-                h=x[9],
+                time=x[11],
+                h=x[10],
                 rot=x[6],
                 p_rot=x[7],
                 desp=x[8]
