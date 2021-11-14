a2108de3495c9594fa2a71d49b1d37c8b26ab8e2
Eziron <kraedon.rf@gmail.com>
2021-11-14 13:54:10 -0300

    test struck

--- joystick/raspberry pi/prueabas.py
+++ joystick/raspberry pi/prueabas.py
@@ -11,16 +11,21 @@ spi.max_speed_hz = 1000000
 
 send_byte = bytearray(73)
 
-ns_ref = time_ns()
-count = 0
-t_sap = 1
-t_sap_ns = t_sap*(10**9)
-while(time_ns()-ns_ref < t_sap_ns):
-    rcv_byte = spi.readbytes(73)
-    #rcv_values = struct.unpack("<"+"l"*17,send_byte[1:73])
-    print("___",count, len(rcv_byte),type(rcv_byte),"_",rcv_byte,"---")
-    count += 1
-    
-print(count)
-print(count/t_sap)
-print((t_sap*1000)/count)
+
+#count = 0
+#t_sap = 0.1
+#t_sap_ns = t_sap*(10**9)
+#ns_ref = time_ns()
+#while(time_ns()-ns_ref < t_sap_ns):
+
+rcv_byte = spi.readbytes(72)
+print(struct.calcsize("l"*17))
+print(len(rcv_byte))
+print(type(rcv_byte))
+print(rcv_byte)
+
+
+#count += 1
+#print(count)
+#print(count/t_sap)
+#print((t_sap*1000)/count)
