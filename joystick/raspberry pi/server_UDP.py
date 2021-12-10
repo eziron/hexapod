7be378dd93cc91773ba02d540e0b1330165c89a1
eziron <kraedon.rf@gmail.com>
2021-12-10 15:08:41 +0100

    test UDP

--- joystick/raspberry pi/server_UDP.py
+++ joystick/raspberry pi/server_UDP.py
@@ -2,7 +2,7 @@ import struct
 from joystick import control_joystick
 import spidev
 from socket import *
-from time import sleep
+from time import sleep, time
 from time import time_ns
 
 spi_bus = 1
@@ -21,11 +21,11 @@ s = socket(AF_INET, SOCK_DGRAM)
 s.connect(('192.168.1.106', 8888))
 
 
-ns_dt = (1/5)*1000000
-ns_ref = time_ns()
+dt = 1/5
+time_ref = time()
 while(True):
-    if(time_ns()-ns_ref >= ns_dt):
-        ns_ref = time_ns()
+    if(time()-time_ref >= dt):
+        time_ref = time()
         val = joystick.read_arduino()
         if(not val is None):
             print(val)
