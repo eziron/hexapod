c4dad3c2d89815f5a2e69702bc8fcdf89cef87a0
Eziron <kraedon.rf@gmail.com>
2022-03-02 12:12:14 -0300

    regreso a sin pwm in

--- hexapod/raspberry_pico/main.py
+++ hexapod/raspberry_pico/main.py
@@ -28,70 +28,6 @@ serial_com = pro_Serial(Serial)
 
 pass_duty = [869, 1560, 1027, 719, 1580, 1215, 939, 1540, 1570, 2031, 1785, 1500, 2131, 1983, 1500, 2061, 1075, 1855]
 
-pwm0= Pin(11,Pin.IN)
-pwm1= Pin(14,Pin.IN)
-pwm2= Pin(15,Pin.IN)
-
-
-duty_in = [0,0,0]
-duty_in_map = [0,0,0]
-ms_ref = [0,0,0]
-
-#11
-#14
-#15
-def a_map(x,in_min,in_max,out_min,out_max): 
-    return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min
-
-def truncar(x,min_val, max_val):
-    if(x < min_val):
-        return min_val
-    elif(x > max_val):
-        return max_val
-    else:
-        return x
-
-def joystick(val:int,min_val:int,max_val:int,cent_val:int,tol:int):
-    val = truncar(val,min_val,max_val)
-    if(val <= cent_val+tol and val >= cent_val-tol):
-        return cent_val
-    else:
-        if(val > cent_val+tol):
-            return round(a_map(val,cent_val+tol,max_val,1500,2000))
-        else:
-            return round(a_map(val,min_val,cent_val-tol,1000,1500))
-
-def pwm0_duty(p):
-    if(pwm0.value()):
-        ms_ref[0] = ticks_us()
-        pwm0.irq(trigger=Pin.IRQ_FALLING , handler=pwm0_duty)
-    else:
-        duty_in[0] = round((duty_in[0]*(4/5))+((ticks_us()-ms_ref[0])/5))
-        duty_in_map[0] = joystick(duty_in[0],1300,1800,1500,60)
-        pwm0.irq(trigger=Pin.IRQ_RISING , handler=pwm0_duty)
-
-def pwm1_duty(p):
-    if(pwm1.value()):
-        ms_ref[1] = ticks_us()
-        pwm1.irq(trigger=Pin.IRQ_FALLING , handler=pwm1_duty)
-        duty_in_map[1] = joystick(duty_in[1],1050,2150,1500,60)
-    else:
-        duty_in[1] = round((duty_in[1]*(4/5))+((ticks_us()-ms_ref[1])/5))
-        pwm1.irq(trigger=Pin.IRQ_RISING , handler=pwm1_duty)
-
-def pwm2_duty(p):
-    if(pwm2.value()):
-        ms_ref[2] = ticks_us()
-        pwm2.irq(trigger=Pin.IRQ_FALLING , handler=pwm2_duty)
-    else:
-        duty_in[2] = round((duty_in[2]*(4/5))+((ticks_us()-ms_ref[2])/5))
-        duty_in_map[1] = joystick(duty_in[2],960,2100,1500,60)
-        pwm2.irq(trigger=Pin.IRQ_RISING , handler=pwm2_duty)
-
-pwm0.irq(trigger=Pin.IRQ_RISING , handler=pwm0_duty)
-pwm1.irq(trigger=Pin.IRQ_RISING , handler=pwm1_duty)
-pwm2.irq(trigger=Pin.IRQ_RISING , handler=pwm2_duty)
-
 while True:
     if(Serial.any()>0):
         tipo,buffer = serial_com.read_command()
@@ -103,7 +39,7 @@ while True:
                         buffer[i] = 0
                 serial_com.send_command(0,"B",buffer)
             elif(tipo == 1 and len(buffer) == 18):
-                serial_com.send_command(1,"H",duty_in)
+                serial_com.send_command(1,"B",[5,5])
                 val = True
                 for x in buffer:
                     if(x < 500 or x > 2500):
